cfb50f5ff5c189906955ecdb0a0bc108291a19db
S566629 <S566629@nwmissouri.edu>
2023-10-31 12:29:59 -0500

    added class

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var instituteRouter = require('./routes/institute');
 
 
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/institute', instituteRouter);
 
 
 
