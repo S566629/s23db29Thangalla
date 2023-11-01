95c4a08fb9160bcf6e3b151f3bfae0edb8c4e86c
S566629 <S566629@nwmissouri.edu>
2023-11-01 01:18:05 -0500

    added images file

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('choose', { title: 'Choose' });
+  res.render('index', { title: 'Choose' });
 });
 
+
+ 
 module.exports = router;
